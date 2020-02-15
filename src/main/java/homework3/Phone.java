1adc00ca90f0dfacb43d597f965f75bd8527c0dc
Benyi Annabella <benyiannabella@yahoo.com>
2020-02-15 16:36:29 +0200

    upload homework3

--- src/main/java/homework3/Phone.java
+++ src/main/java/homework3/Phone.java
@@ -3,21 +3,21 @@ package homework3;
 import java.util.List;
 
 public interface Phone {
-    // all the homework requirements have been developed only under sublass SamsungGalaxyS6;
-    // where the below methods have been overriden to fulfill the requirements;
+    //  the below methods have been override under sublass SamsungGalaxyS6 to fulfill the requirements;
+    //  subclass SGS9 and Nokia classes have been created because the homework requested two brands and two models each, no extra functions developed;
 
     // add contact to the contact list;
-    public Contact[] addContact(Contact contactToAdd, Contact[] phoneContact);
+    Contact[] addContact(Contact contactToAdd, Contact[] phoneContact);
     //display contact list for the selected phone;
-    public void viewContactList(Contact[] newContactList);
+    void viewContactList(Contact[] newContactList);
     //display all the messages successfully sent to a selected contact;
-    public void listMessage(List <Message> messageSent, Contact contact);
+    void listMessage(List <Message> messageSent, Contact contact);
     //check and override phone battery life at after every message sent and call made
-    public void setCurrentBatteryLife(boolean message);
+    void setCurrentBatteryLife(boolean message);
     //check if message can be sent, if exceeds 500 char -  block;
-    public void checkIfSendMessage(Message messageToSend, Phone phone, List<Message> messageSent, Contact contact);
+    void checkIfSendMessage(Message messageToSend, Phone phone, List<Message> messageSent, Contact contact);
     //call a selected contact;
-    public Call[] makeACall(Call callToMake, Call[] callMade, Phone phone);
+    Call[] makeACall(Call callToMake, Call[] callMade, Phone phone);
     //display the call History of the selected phone;
-    public void viewCallHistory(Call[] callMade);
+    void viewCallHistory(Call[] callMade);
 }
