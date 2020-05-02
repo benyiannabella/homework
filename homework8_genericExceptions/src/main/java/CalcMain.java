047e67fccb747f3474edd8dddf9e2f8d83c15a43
Benyi Annabella <benyiannabella@yahoo.com>
2020-05-03 00:02:12 +0100

    upload

--- homework8_genericExceptions/src/main/java/CalcMain.java
+++ homework8_genericExceptions/src/main/java/CalcMain.java
@@ -19,7 +19,7 @@ public class CalcMain {
         //Number result = verify.calculate(s);
 
         //integer calculator validates parentheses in simple equations;
-        //like 1+(2*3) or (4*5)/5 or 10-(2*2)+5;
+        //like 1+(2*3) or (4*5)/5 or 10-(2*2)/5;
         Helper2 verify2 = new Helper2();
         Number result2 = verify2.calculate2(s);
 
